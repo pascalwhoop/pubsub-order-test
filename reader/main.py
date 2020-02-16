56224f3fca27c9e4d501ad4d29386909f7fe83f9
Pascal Brokmeier <accounts@pascalbrokmeier.de>
2020-02-16 12:13:59 +0100

    add state getter

--- reader/main.py
+++ reader/main.py
@@ -1,53 +1,57 @@
-# Copyright 2018 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# [START gae_python37_app]
-from flask import Flask, request, Response
+from flask import Flask, request, Response, jsonify
 import json
+from json import JSONEncoder
+import logging as log
+import base64
+from dataclasses import dataclass
+from collections import defaultdict
+
 
 
 # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
 # called `app` in `main.py`.
 app = Flask(__name__)
 
+@dataclass
 class State:
-    def __init__(self):
-        self.current_stage = "start"
-        self.stage_stats = {}
+    current_stage: str
+    stage_stats: dict
     
+@dataclass
 class StageStats:
-    def __init__(self):
-        self.out_of_index_count=0
-        self.out_of_order_count=0
-        self.total_received=0
-        self.last_counter=0
+    out_of_index_count: int=0
+    out_of_order_count: int=0
+    total_received    : int=0
+    last_counter      : int=0
 
-state = State()
+state: State = None
+def _reset_state():
+    global state
+    state = State(current_stage= "start", stage_stats={})
+_reset_state()
+
+
+@app.route('/state', methods=["GET"])
+def get_state():
+    return Response(json_encoder.encode(state), mimetype="application/json")
 
 @app.route('/push', methods=["POST"])
 def push():
-    msg = _handle_message_json(request.json)
-    return _handle_inner_message(msg)
+    if request.json is None:
+        return Response(status=400)
+    else:
+        msg = _handle_message_json(request.json)
+        return _handle_inner_message(msg)
     
     
     
 def _handle_inner_message(msg):
     if msg["type"] == "start":
-        pass
+        log.warning("received start message")
     elif msg["type"] == "end":
+        log.warning("received start end")
         # TODO make statistics
-        pass
+        log.info(json_encoder.encode(state))
     elif msg["type"] == "step":
         _validate_ready(msg["stage"])
         _handle_step(msg)
@@ -71,15 +75,22 @@ def _handle_step(msg):
     # messages are out of order if they are not in their right index
     if now_counter != stats.total_received:
         stats.out_of_index_count += 1
+        log.warning(f"OOIC ++ : msg counter: {now_counter}, system counter: {stats.total_received}")
     if stats.last_counter +1 != now_counter:
         stats.out_of_order_count += 1
+        log.warning(f"OOOC ++ : last received: {stats.last_counter}, msg counter: {now_counter}")
     
     stats.last_counter = now_counter
     
 
+class DataClassEncoder(JSONEncoder):
+        def default(self, o):
+            return o.__dict__
+json_encoder = DataClassEncoder()
 
 def _handle_message_json(msg_json) -> dict:
-    json.loads(msg_json["message"]["data"].decode("utf-8"))
+    base_bytes = msg_json["message"]["data"].encode()
+    return json.loads(base64.decodebytes(base_bytes).decode("utf-8"))
 
 
 def _validate_ready(stage):
@@ -87,9 +98,6 @@ def _validate_ready(stage):
         state.stage_stats[stage] = StageStats()
 
 
-def _reset_state():
-    global state
-    state = State()
 
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
--- reader/test_main.py
+++ reader/test_main.py
@@ -14,9 +14,18 @@
 
 import main
 import unittest
-STAGE = 1
+import json
+from flask import Response
+import os
+from unittest.mock import patch
+STAGE = 0
 
 class TestMain(unittest.TestCase):
+
+    def test_get_state(self):
+        res = main.get_state()
+        assert isinstance(res, Response)
+        assert res.get_json()["current_stage"] == "start"
 
     def test_handle_step(self):
         assert main.state.stage_stats.get(STAGE) is None
@@ -36,18 +45,58 @@ class TestMain(unittest.TestCase):
         assert main.state.stage_stats[STAGE].out_of_order_count == 2
         main._reset_state()
 
-    def test_handle_whole_flow(self):
-        pass
-        # start
-        # stage 1
-        # stage 2
-        # stop
-        # validate stats are there
+    def test_handle_inner_message_start(self):
+        # test start resets
+        msg = {
+            "type": "start"
+        }
+        a = main.state
+        main._handle_inner_message(msg)
+        b = main.state
+        assert a is b
+    
+    @patch("main.log")
+    def test_handle_inner_message_end(self, log_mock):
+        # test start resets
+        msg = {
+            "type": "end"
+        }
+        main._handle_inner_message(msg)
+        assert log_mock.info.call_count == 1
+        assert log_mock.warning.call_count == 1
 
-def _step_message(i):
+    def test_handle_message_json(self):
+        msg = json.load(open(rel_path(__file__, "test_request.json")))
+        inside_message = main._handle_message_json(msg)
+        print(inside_message)
+        assert isinstance(inside_message, dict)
+        assert "==" not in inside_message
+
+
+    def test_handle_whole_flow(self):
+        [main._handle_inner_message(_step_message(i, 0)) for i in range(1,50)]
+        assert main.state.stage_stats[0].out_of_index_count == 0
+        [main._handle_inner_message(_step_message(i, 1)) for i in range(1,50)]
+        assert main.state.stage_stats[1].out_of_index_count == 0
+        [main._handle_inner_message(_step_message(i, 2)) for i in range(1,50)]
+        assert main.state.stage_stats[2].out_of_index_count == 0
+        [main._handle_inner_message(_step_message(i, 3)) for i in range(1,50)]
+        assert main.state.stage_stats[3].out_of_index_count == 0
+        assert len(main.state.stage_stats) == 4
+
+def _step_message(i, stage=0):
     return {
-        "stage": STAGE,
+        "stage": stage,
         "counter": i,
         "sleep_length": 0,
         "type": "step"
     }
+
+
+def dir_path(file: str) -> str:
+    return os.path.dirname(os.path.abspath(file))
+
+
+def rel_path(file: str, relative_path: str) -> str:
+    dir_ = dir_path(file)
+    return os.path.abspath(os.path.join(dir_, relative_path))
